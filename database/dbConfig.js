5fa785cc286e8999edea88f6dfcffd3418168a56
Your Name <you@example.com>
2020-07-03 10:52:25 -0700

    connected with postefresql database

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,5 +1,7 @@
 const knex = require('knex');
 
+require('dotenv').config()
+
 const environment = process.env.DB_ENV || process.env.NODE_ENV || "development";
 
 const config = require("../knexfile")[environment];
