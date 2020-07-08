40a7f1b5d82ff23e10469ca09f780dac11d8e00b
Your Name <you@example.com>
2020-07-08 14:37:30 -0700

    reconfiged cloudinary

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -7,14 +7,14 @@ const {
     getImagesByHouseId
 } = require('../queries/imageQueries')
 
-const cloud_name = `${process.env.CLOUD_NAME}`
-const api_key = `${process.env.API_KEY}`
-const api_secret = `${process.env.API_SECRET}`
+const cloud_name = process.env.CLOUD_NAME
+const api_key = process.env.API_KEY
+const api_secret = process.env.API_SECRET
 
 cloudinary.config({
-    cloud_name: cloud_name,
-    api_key: api_key,
-    api_secret: api_secret
+    cloud_name: `${cloud_name}`,
+    api_key: `${api_key}`,
+    api_secret: `${api_secret}`
 })
 
 
