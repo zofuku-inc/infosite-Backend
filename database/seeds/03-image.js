d59d3f2f0cdb541a3c2c7b0b657e0ffdb07f66a1
Your Name <you@example.com>
2020-07-24 16:08:46 -0700

    remigrated to add income and cost column

--- database/seeds/03-image.js
+++ database/seeds/03-image.js
@@ -7,7 +7,7 @@ exports.seed = function(knex) {
       return knex('image').insert([
         {
           id: 1, 
-          image_url: 'https://www.publicdomainpictures.net/pictures/320000/velka/background-image.png',
+          image_url: 'https://res.cloudinary.com/zofuku/image/upload/v1595627524/house_default_zg7uwu.jpg',
           width: 400,
           height: 300
         },
