610594adf27ba1787a012548840e4a45e94d5214
Your Name <you@example.com>
2020-09-03 21:05:58 -0700

    tried to specify credential on the header

--- app.js
+++ app.js
@@ -46,6 +46,14 @@ const imageRoutes = require('./api/images/imageRoutes');
 //middlewares
 // app.set('trust proxy', 1);
 // app.enable('trust proxy');
+app.use(function(req, res, next) {
+        res.header('Access-Control-Allow-Credentials', true);
+        // res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+        next();
+    }
+);
 app.use(sslRedirect());
 app.use(cookieParser());
 app.use(bodyParser.json());
