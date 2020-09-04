172a96f56c8a9e66354bd3972956a05def08e0b0
Your Name <you@example.com>
2020-09-03 21:13:48 -0700

    added allow origin

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const imageRoutes = require('./api/images/imageRoutes');
 // app.enable('trust proxy');
 app.use(function(req, res, next) {
         res.header('Access-Control-Allow-Credentials', true);
-        // res.header('Access-Control-Allow-Origin', req.headers.origin);
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
         next();
