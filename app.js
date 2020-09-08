cd31a29434b5320cdcb0071756da724d8162d7cf
Your Name <you@example.com>
2020-09-08 09:29:27 -0700

    try proxy 0

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const housingRoutes = require('./api/houseSelling/houseRoutes');
 const userRoutes = require('./api/users/userRoutes');
 const imageRoutes = require('./api/images/imageRoutes');
 
-app.set('trust proxy', 1);
+app.set('trust proxy', 0);
 app.use(sslRedirect());
 app.use(cookieParser());
 app.use(bodyParser.json());
