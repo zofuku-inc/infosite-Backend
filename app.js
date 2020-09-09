8779362bf4884e8fa97722f477632823ec526a3b
Your Name <you@example.com>
2020-09-09 11:23:07 -0700

    changed secure to false

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ const sessionConfig = {
     secret: 'keep it secret, keep it safe!',
     cookie: {
         maxAge: 1000*60*60*2 ,
-        secure: app.get('env') === 'production', // only set cookies over https. Server will not send back a cookie over http.
-        domain: 'https://store.spaceincome.jp'
+        secure: false, // only set cookies over https. Server will not send back a cookie over http.
+        domain: 'store.spaceincome.jp'
     },
     httpOnly: true, // don't let JS code access cookies. Browser extensions run JS code on your browser!
     resave: false,
